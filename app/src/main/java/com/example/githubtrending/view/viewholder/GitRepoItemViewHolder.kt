72b978057a5242f39dbe8a35daf8082c2f8096d5
Devesh Avutu <devesh14may@gmail.com>
2020-01-09 23:24:31 +0530

    displaying profile images in circle shape using circleImageView library

--- app/src/main/java/com/example/githubtrending/view/viewholder/GitRepoItemViewHolder.kt
+++ app/src/main/java/com/example/githubtrending/view/viewholder/GitRepoItemViewHolder.kt
@@ -5,8 +5,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.githubtrending.R
 import com.example.githubtrending.network.GitHubRepoData
+import de.hdodenhof.circleimageview.CircleImageView
 
 class GitRepoItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
@@ -20,9 +22,17 @@ class GitRepoItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) 
 
     private val authorName: TextView = itemView.findViewById(R.id.author_name)
     private val gitHubRepoName: TextView = itemView.findViewById(R.id.repo_name)
+    private val avatar: CircleImageView = itemView.findViewById(R.id.avatar)
 
     fun bind(item: GitHubRepoData) {
         authorName.text = item.author
         gitHubRepoName.text = item.name
+        loadImage(item.avatar)
+    }
+
+    private fun loadImage(url: String) {
+        Glide.with(avatar.context)
+            .load(url)
+            .into(avatar)
     }
 }
